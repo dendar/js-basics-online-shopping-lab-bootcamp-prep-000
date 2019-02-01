60003b3a9e3763a1b69741887c33882fe29feb50
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-02-01 01:17:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,20 +11,61 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price =Math.floor(Math.random()*100)+1
+ var itemObject ={}
+ itemObject[item] =price
+ cart.push(itemObject)
+ console.log(item + " has been added to your cart.")
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length ===0) {
+    console.log("Your shopping cart is empty.")
+
+  }else{
+    var items =[]
+    for(var i=0; i<cart.length; i++){
+      for(var item in cart[i]){
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(", "))
+  }
 }
 
 function total() {
   // write your code here
+  let t =0
+  return t
 }
 
 function removeFromCart(item) {
   // write your code here
+  var itemCart =false
+  for(var i=0; i<cart.length; i++){
+    if (cart[i].hasOwnProperty(item)) {
+    itemCart=true
+    cart.splice(i, 1)
+  }
+  }
+  if (!itemCart) {
+    console.log("That item is not in your cart.")
+
+  }
+  return cart
 }
+
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (!creditCard) {
+    console.log("We don't have a credit card on file for you to place your order.")
+
+  }
+  else{
+    console.log("Your total cost is $" +total()+ ", which will be chard to the card " +creditCard + ".")
+    cart = []
+  }
 }
